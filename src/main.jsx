6d356e4a72e8ea3766f8ccf0c6ebe9e5e928885c
廖冠博 <h2itwh2itw@gmail.com>
2025-02-23 20:58:37 +0800

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import { StrictMode } from 'react'
+import './assets/scss/all.scss'
 import { createRoot } from 'react-dom/client'
-// import './index.css'
 import App from './App.jsx'
 import 'bootstrap'
-import './assets/all.scss'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
