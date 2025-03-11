dbc07b8640953848d3ba9dcffbab2cf73f125674
yingyingw325 <yingyingw325@gmail.com>
2025-03-11 17:49:32 +0800

    0311 detail頁面部分版型

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect, useRef } from 'react';
 import { Routes, Route } from 'react-router';
 import Home from './pages/Home';
 import Products from './pages/Products';
+import ProductDetail from './pages/ProductDetail';
 import './assets/scss/all.scss'
 
 function App() {
@@ -10,6 +11,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>}></Route>
         <Route path='/products' element={<Products/>}></Route>
+        
+        <Route path="/products/:productId" element={<ProductDetail />} />
+
       </Routes>
     </>
   )
